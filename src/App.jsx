5fa3f1d766eecd4058e2df641a70e05113344925
Juan Pablo Velez <juan.velez993@gmail.com>
2022-04-29 11:35:16 -0500

    FIX---proyect setup on my computer and create component list

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import FormOfStudents from './components/FormOfStudents'
 import ListOfStudents from './components/ListOfStudents'
 
 function App() {
 
-  const [number, setNumber] = useState(0)
-
-  const addToNumber = ()=>{
-    setNumber(number + 1)
-  }
-
-  const substractToNumber = ()=>{
-    setNumber(number - 1)
-  }
-
   return (
     <div>
       <h1>My first application</h1>
-      <h1>{number}</h1>
-      <button onClick={addToNumber}>+</button>
-      <button onClick={substractToNumber}>-</button>
+      <FormOfStudents />
       <ListOfStudents />
     </div>
   )
